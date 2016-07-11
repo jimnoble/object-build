04c3ebb99f7d6bea777ecc9561981b9f23cd3788
Jim Noble <none@none>
2016-07-11 01:12:02 -0400

    update assembly infos

--- Object.Build.Tests/Properties/AssemblyInfo.cs
+++ Object.Build.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Immutable.Build.Tests")]
+[assembly: AssemblyTitle("Object.Build.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Immutable.Build.Tests")]
+[assembly: AssemblyProduct("Object.Build.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
